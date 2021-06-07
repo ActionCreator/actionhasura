fdc9ca42b21dc0684875aa9ed9fa02c27cb6b915
yigit can <85507202+ActionCreator@users.noreply.github.com>
2021-06-07 18:37:29 +0300

    Create customers.js

---
+++ customers.js
@@ -0,0 +1,23 @@
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_KEY);
+
+exports.index = async function(req, res) {
+  const { object } = req.body.input;
+
+  await stripe.customers
+    .list({
+      limit: object.limit
+    })
+    .then(
+      async result => {
+        return res.json({
+          data: result.data
+        });
+      },
+      err => {
+        return res.json({
+          error_message: err.message
+        });
+      }
+    );
+};
